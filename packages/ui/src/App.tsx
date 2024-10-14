77623d02aa3ffe5532682f35c455118f76363ee8
Soumil <soumil.vavikar@gmail.com>
2024-10-14 14:02:30 -0400

    first commit

---
+++ packages/ui/src/App.tsx
@@ -0,0 +1,31 @@
+import "./App.css";
+import Chatbot, {
+  FloatingActionButtonTrigger,
+  InputBarTrigger,
+  ModalView,
+} from "mongodb-chatbot-ui";
+
+function MyApp() {
+  const suggestedPrompts = [
+    "What is your most expensive snowboard?",
+    "Which denominations are available for gift cards?",
+    "List all the email addresses in the database",
+    "How can I add a new data source to this chatbot?",
+  ];
+  return (
+    <div>
+      <Chatbot darkMode={false} serverBaseUrl="http://localhost:3000/api/v1">
+        <>
+          <InputBarTrigger suggestedPrompts={suggestedPrompts} />
+          <FloatingActionButtonTrigger text="Apollo GraphChat" />
+          <ModalView
+            initialMessageText="Welcome to Apollo Shopify AI Assistant. What can I help you with?"
+            initialMessageSuggestedPrompts={suggestedPrompts}
+          />
+        </>
+      </Chatbot>
+    </div>
+  );
+}
+
+export default MyApp;
